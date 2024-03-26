4660069c04dcf6764aafbfe40d01f36ead616eb9
unknown <nagaraju.tananki@gmail.com>
2024-03-26 19:14:11 +0530

    new component

---
+++ src/app/cm/cm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cm',
+  standalone: true,
+  imports: [],
+  templateUrl: './cm.component.html',
+  styleUrl: './cm.component.css'
+})
+export class CmComponent {
+
+}
